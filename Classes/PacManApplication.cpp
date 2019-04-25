e8d5a8e581da9140fe963268d56fa88f8e7896aa
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2019-04-25 10:27:25 +0200

    Change default window size to medium for debug purposes

--- Classes/PacManApplication.cpp
+++ Classes/PacManApplication.cpp
@@ -83,7 +83,10 @@ bool PacManApplication::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-		glview = GLViewImpl::createWithFullScreen("MetroidPacMan");
+		// DEBUG
+		glview = GLViewImpl::createWithRect("MetroidPacMan", cocos2d::Rect(0, 0, mediumResolutionSize.width, mediumResolutionSize.height), 1, true);
+		
+		//glview = GLViewImpl::createWithFullScreen("MetroidPacMan");
 #else
         glview = GLViewImpl::create("MetroidPacMan");
 #endif
