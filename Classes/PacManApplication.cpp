f47f3628856c95bbdef76cd828413015f0154787
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2019-04-25 12:38:09 +0200

    Change design resolution to 1440x1080

--- Classes/PacManApplication.cpp
+++ Classes/PacManApplication.cpp
@@ -42,10 +42,10 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(1920, 1080);
+static cocos2d::Size designResolutionSize = cocos2d::Size(1440, 1080);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(640, 480);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(1920, 1080);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(1440, 1080);
 
 PacManApplication::PacManApplication()
 {
