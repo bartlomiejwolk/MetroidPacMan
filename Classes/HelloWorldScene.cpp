dec2e67b9fbe3d8789ca2fd8d986cce6e5197df3
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2019-04-25 15:38:28 +0200

    Align ghost name labels

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -166,6 +166,7 @@ void HelloWorld::DrawHighScoreLabel()
 void HelloWorld::DrawCharacterNicknames()
 {
 	Label *label = nullptr;
+	
 	float x = 0;
 	float y = 0;
 
@@ -183,48 +184,52 @@ void HelloWorld::DrawCharacterNicknames()
 
 	// draw OIKAKE----"AKABEI"
 	label = Label::createWithTTF("OIKAKE----\"AKABEI\"", "fonts/arial.ttf", 70);
+	label->setAnchorPoint(Vec2(0, 0));
 	if (label == nullptr)
 	{
 		problemLoading("'fonts/Marker Felt.ttf'");
 		return;
 	}
-	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width / 2;
+	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width * 0.4;
 	y = m_OpenGLOrigin.y + m_OpenGLVisibleSize.height * 0.7;
 	label->setPosition(Vec2(x, y));
 	this->addChild(label, 1);
 
 	// draw MACHIBUSE----"PINKY"
 	label = Label::createWithTTF("MACHIBUSE----\"PINKY\"", "fonts/arial.ttf", 70);
+	label->setAnchorPoint(Vec2(0, 0));
 	if (label == nullptr)
 	{
 		problemLoading("'fonts/Marker Felt.ttf'");
 		return;
 	}
-	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width / 2;
+	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width * 0.4;
 	y = m_OpenGLOrigin.y + m_OpenGLVisibleSize.height * 0.65;
 	label->setPosition(Vec2(x, y));
 	this->addChild(label, 1);
 
 	// draw KIMAGURE----"AOSUKE"
 	label = Label::createWithTTF("KIMAGURE----\"AOSUKE\"", "fonts/arial.ttf", 70);
+	label->setAnchorPoint(Vec2(0, 0));
 	if (label == nullptr)
 	{
 		problemLoading("'fonts/Marker Felt.ttf'");
 		return;
 	}
-	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width / 2;
+	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width * 0.4;
 	y = m_OpenGLOrigin.y + m_OpenGLVisibleSize.height * 0.6;
 	label->setPosition(Vec2(x, y));
 	this->addChild(label, 1);
 
 	// draw OTOBOKE----"GUZUTA"
 	label = Label::createWithTTF("OTOBOKE----\"GUZUTA\"", "fonts/arial.ttf", 70);
+	label->setAnchorPoint(Vec2(0, 0));
 	if (label == nullptr)
 	{
 		problemLoading("'fonts/Marker Felt.ttf'");
 		return;
 	}
-	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width / 2;
+	x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width * 0.4;
 	y = m_OpenGLOrigin.y + m_OpenGLVisibleSize.height * 0.55;
 	label->setPosition(Vec2(x, y));
 	this->addChild(label, 1);
