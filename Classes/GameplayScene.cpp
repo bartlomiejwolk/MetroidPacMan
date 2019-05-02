00102a6ecf2680d7523eff4e7c84eb00abf00e83
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2019-05-02 13:15:31 +0200

    Create GameplayScene class

---
+++ Classes/GameplayScene.cpp
@@ -0,0 +1,82 @@
+#include "GameplayScene.h"
+#include "SimpleAudioEngine.h"
+#include "Utils.h"
+
+USING_NS_CC;
+
+Scene* GameplayScene::createScene()
+{
+    return GameplayScene::create();
+}
+
+// TODO move to Utils namespace
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool GameplayScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Scene::init() )
+    {
+        return false;
+    }
+
+    m_OpenGLVisibleSize = Director::getInstance()->getVisibleSize();
+    m_OpenGLOrigin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(GameplayScene::menuCloseCallback, this));
+
+    if (closeItem == nullptr ||
+        closeItem->getContentSize().width <= 0 ||
+        closeItem->getContentSize().height <= 0)
+    {
+        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+    }
+    else
+    {
+		Size contentSize = closeItem->getContentSize();
+        float x = m_OpenGLOrigin.x + m_OpenGLVisibleSize.width - contentSize.width/2;
+        float y = m_OpenGLOrigin.y + contentSize.height/2;
+        closeItem->setPosition(Vec2(x,y));
+    }
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+
+    return true;
+}
+
+void GameplayScene::menuCloseCallback(Ref* pSender)
+{
+    //Close the cocos2d-x game scene and quit the application
+    Director::getInstance()->end();
+
+    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+
+    //EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+
+}
---
+++ Classes/GameplayScene.h
@@ -0,0 +1,38 @@
+#ifndef __GAMEPLAY_SCENE_H__
+#define __GAMEPLAY_SCENE_H__
+
+#include "cocos2d.h"
+
+class GameplayScene : public cocos2d::Scene
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(GameplayScene);
+
+private:
+	// TODO make global
+	// Visible size of the OpenGL view in points.
+	// Initialized in `init()`.
+	cocos2d::Size m_OpenGLVisibleSize;
+
+	// TODO make global
+	// Visible origin coordinate of the OpenGL view in points.
+	// Initialized in `init()`.
+	cocos2d::Vec2 m_OpenGLOrigin;
+
+	// Font used for all text in the game.
+	const std::string m_FontFilePath = "fonts/GameCube.ttf";
+
+	// Font size used for all text in the game.
+	const int m_FontSize = 70;
+
+};
+
+#endif // __GAMEPLAY_SCENE_H__
